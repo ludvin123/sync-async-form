5fd35d9d41a9b4651df061d4f1212a18f2149e4d
Alumno04 J601 <a04j601@campustls.local>
2017-11-14 12:11:01 -0500

    agregar modificaciones js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,29 @@
-$(document).ready(function () {
+$(document).ready(function (){
+ $('input[type="submit"]').on("click", function(event){
+   event.preventDefault();
+   var first_name = $('#first_name').val(); // [first_name]
+   var last_name = $('#last_name').val(); // [last_name]
+   var gender = $('input[name="gender"]:checked').val(); // []
+   var interest = $('input[name="interest"]:checked'); // [1,2]
+   var interests = new Array();
 
-  $('input[type="submit"]').on ("click", function(event){
-  var first_name = $('input[name="first_name"]').val();
-  console.log(first_name);
+   for (var i = 0; i < interest.length; i++) {
+     var temp_interest = interest[i];
 
-  event.preventDefault();
+    interests.push(temp_interest.value);
+   }
+  /* console.log(interest.val());*/
+ var payload = {
+   'first_name':first_name,
+   'last_name':last_name,
+   'gender':gender ,
+   'interest':interests
+ }
+
+ $.post("find.html", payload);
+
+
+ /*$.post('find.html',payload);
+ $.get('find.html',payload);*/
+ });
 });
-})
